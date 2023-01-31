1e7209e3d57e97b8d3d54771bab661d8e3161873
Wandonium <hillarywando@gmail.com>
2023-01-31 20:31:10 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        // specify value_f as a float value
+        float value_f = 9f;
+        // specify value_d as a double value
+        double value_d = 10d;
+
+        // use specific character to mark floating point value
+        // in calculation:
+        int number = 512;
+        double percentage = number * 0.46f;
+        System.out.println("46% of " + number + " is: " + percentage);
+
+    }
+}
