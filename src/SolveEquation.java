ed25d35b515830614641ddb257b41e9fa56603ba
Wandonium <hillarywando@gmail.com>
2023-02-02 21:00:51 +0300

    Added int version of solving (a+b)squared equation.

---
+++ src/SolveEquation.java
@@ -0,0 +1,22 @@
+public class SolveEquation {
+
+    public static void main(String[] args) {
+        // create a program for solving the equation
+        // (a+b)square = ((a*a) + 2ab + (b*b));
+        int a =
+    }
+
+    public int int_aPlus_bSquared(int a, int b) {
+        int aSquared = a * a;
+        int bSquared = b * b;
+        int mid = 2 * a * b;
+        return aSquared + bSquared + mid;
+    }
+
+    public int int_aPlus_bSquared(int a, int b) {
+        int aSquared = a * a;
+        int bSquared = b * b;
+        int mid = 2 * a * b;
+        return aSquared + bSquared + mid;
+    }
+}
