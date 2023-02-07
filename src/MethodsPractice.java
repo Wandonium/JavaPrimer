96609417da16e6ca7aa19af5fff2af3eaf293d06
Wandonium <hillarywando@gmail.com>
2023-02-07 20:32:13 +0300

    Added comments to MethodsPractice class.

--- src/MethodsPractice.java
+++ src/MethodsPractice.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class MethodsPractice {
 
+    // practicing use of methods in Java.
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter length of rectangle: ");
