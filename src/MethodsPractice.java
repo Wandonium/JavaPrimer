5970c2ab3e95869efb34b869b98af224c650c0bb
Wandonium <hillarywando@gmail.com>
2023-02-07 20:41:12 +0300

    Added other area methods in MethodsPractice class to illustrate method overloading in Java.

--- src/MethodsPractice.java
+++ src/MethodsPractice.java
@@ -10,9 +10,27 @@ public class MethodsPractice {
         System.out.print("Enter width of rectangle: ");
         double width = scanner.nextDouble();
         System.out.println("Area of rectangle is: " + area(length, width));
+        System.out.println("Area of rectangle of length 14.25 and width 10.65 is: " +
+                area(14.25d, 10.65d));
+        System.out.println("Area of rectangle of length 14 and width 10 is: " +
+                area(14, 10));
+        System.out.println("Area of rectangle of length 14 and width 14 is: " +
+                area(14));
     }
 
     public static double area(double length, double width) {
         return length * width;
+    }
+
+    // Illustrating method overloading in Java using different
+    // parameter types.
+    public static int area(int length, int width) {
+        return length * width;
+    }
+
+    // Illustrating method overloading in Java using different
+    // number of parameters
+    public static double area(double length) {
+        return length * length;
     }
 }
