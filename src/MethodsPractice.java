f1965e9584edfff73084752d6879e312a372a133
Wandonium <hillarywando@gmail.com>
2023-02-07 20:53:53 +0300

    Added checking for prime numbers in both integers and floats in MethodsPractice class.

--- src/MethodsPractice.java
+++ src/MethodsPractice.java
@@ -16,6 +16,8 @@ public class MethodsPractice {
                 area(14, 10));
         System.out.println("Area of rectangle of length 14 and width 14 is: " +
                 area(14));
+        System.out.println("Is 7 a prime number? " + isPrime(7));
+        System.out.println("Is 7.0 a prime number? " + isPrime(7.0f));
     }
 
     public static double area(double length, double width) {
@@ -32,5 +34,22 @@ public class MethodsPractice {
     // number of parameters
     public static double area(double length) {
         return length * length;
+    }
+
+    public static boolean isPrime(int num) {
+        for(int i = 2; i <= num/2; i++) {
+            if(num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static boolean isPrime(float num) {
+        for(int i = 2; i <= num/2; i++) {
+            if(num % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
