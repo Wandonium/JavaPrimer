e511fd2dd4b8dd713bc38321de8267da6d625abf
Wandonium <hillarywando@gmail.com>
2023-02-07 20:35:04 +0300

    Formatted the user input instructions in the console app in MethodsPractice class.

--- src/MethodsPractice.java
+++ src/MethodsPractice.java
@@ -7,9 +7,9 @@ public class MethodsPractice {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter length of rectangle: ");
         double length = scanner.nextDouble();
-        System.out.print("\nEnter width of rectangle: ");
+        System.out.print("Enter width of rectangle: ");
         double width = scanner.nextDouble();
-        System.out.println("\nArea of rectangle is: " + area(length, width));
+        System.out.println("Area of rectangle is: " + area(length, width));
     }
 
     public static double area(double length, double width) {
