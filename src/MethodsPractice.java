a33c426a2d63963f2b74db4dcf6b4554a96d5c8f
Wandonium <hillarywando@gmail.com>
2023-02-07 20:33:31 +0300

    Added spaces to user input instructions in MethodsPractice class.

--- src/MethodsPractice.java
+++ src/MethodsPractice.java
@@ -5,11 +5,11 @@ public class MethodsPractice {
     // practicing use of methods in Java.
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter length of rectangle: ");
+        System.out.print("Enter length of rectangle: ");
         double length = scanner.nextDouble();
-        System.out.println("Enter width of rectangle: ");
+        System.out.print("\nEnter width of rectangle: ");
         double width = scanner.nextDouble();
-        System.out.println("Area of rectangle is: " + area(length, width));
+        System.out.println("\nArea of rectangle is: " + area(length, width));
     }
 
     public static double area(double length, double width) {
