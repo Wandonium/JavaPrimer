e13cc6d65624c5a7869b20d1600e73406bf61b04
Wandonium <hillarywando@gmail.com>
2023-02-08 20:33:01 +0300

    Added the Reptile class.

---
+++ src/OOP/Reptile.java
@@ -0,0 +1,33 @@
+package OOP;
+
+public class Reptile extends Animal{
+
+    private boolean drySkin;
+    private boolean backbone;
+    private String eggType = "soft-shelled";
+
+    public Reptile(double height, double weight, String type, String bloodType, boolean drySkin, boolean backbone) {
+        super(height, weight, type, bloodType);
+        this.drySkin = drySkin;
+        this.backbone = backbone;
+    }
+
+    public Reptile() {
+    }
+
+    public boolean isDrySkin() {
+        return drySkin;
+    }
+
+    public void setDrySkin(boolean drySkin) {
+        this.drySkin = drySkin;
+    }
+
+    public boolean isBackbone() {
+        return backbone;
+    }
+
+    public void setBackbone(boolean backbone) {
+        this.backbone = backbone;
+    }
+}
