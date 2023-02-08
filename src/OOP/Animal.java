2632a101be1d00629f44c0c1ab009e4b4f965037
Wandonium <hillarywando@gmail.com>
2023-02-08 20:30:03 +0300

    Added the Animal class in the OOP package to illustrate OOP Inheritance concepts.

---
+++ src/OOP/Animal.java
@@ -0,0 +1,53 @@
+package OOP;
+
+public class Animal {
+
+    private double height;
+    private double weight;
+    private String type;
+    private String bloodType;
+
+    public Animal(double height, double weight, String type, String bloodType) {
+        this.height = height;
+        this.weight = weight;
+        this.type = type;
+        this.bloodType = bloodType;
+    }
+
+    public Animal() {
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBloodType() {
+        return bloodType;
+    }
+
+    public void setBloodType(String bloodType) {
+        this.bloodType = bloodType;
+    }
+
+
+}
