240cacd432b9108107dbaa657154da8e84b568fe
Wandonium <hillarywando@gmail.com>
2023-02-08 20:36:40 +0300

    Added the Bird class in OOP package.

---
+++ src/OOP/Bird.java
@@ -0,0 +1,32 @@
+package OOP;
+
+public class Bird extends Animal {
+
+    private boolean hasFeathers;
+    private boolean canFly;
+
+    public Bird(double height, double weight, String type, String bloodType, boolean hasFeathers, boolean canFly) {
+        super(height, weight, type, bloodType);
+        this.hasFeathers = hasFeathers;
+        this.canFly = canFly;
+    }
+
+    public Bird() {
+    }
+
+    public boolean isHasFeathers() {
+        return hasFeathers;
+    }
+
+    public void setHasFeathers(boolean hasFeathers) {
+        this.hasFeathers = hasFeathers;
+    }
+
+    public boolean isCanFly() {
+        return canFly;
+    }
+
+    public void setCanFly(boolean canFly) {
+        this.canFly = canFly;
+    }
+}
