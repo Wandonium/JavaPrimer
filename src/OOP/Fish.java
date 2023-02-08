ccbffebbbfa3424b90382135bfd8a99b9c427110
Wandonium <hillarywando@gmail.com>
2023-02-08 20:34:39 +0300

    Added Fish class in OOP package.

---
+++ src/OOP/Fish.java
@@ -0,0 +1,32 @@
+package OOP;
+
+public class Fish extends Animal {
+
+    private boolean livesInWater;
+    private boolean hasGills;
+
+    public Fish(double height, double weight, String type, String bloodType, boolean livesInWater, boolean hasGills) {
+        super(height, weight, type, bloodType);
+        this.livesInWater = livesInWater;
+        this.hasGills = hasGills;
+    }
+
+    public Fish() {
+    }
+
+    public boolean isLivesInWater() {
+        return livesInWater;
+    }
+
+    public void setLivesInWater(boolean livesInWater) {
+        this.livesInWater = livesInWater;
+    }
+
+    public boolean isHasGills() {
+        return hasGills;
+    }
+
+    public void setHasGills(boolean hasGills) {
+        this.hasGills = hasGills;
+    }
+}
