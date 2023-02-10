b60c942b9c0902e8fc628d54f71e3c4787802cf2
Wandonium <hillarywando@gmail.com>
2023-02-10 05:54:21 +0300

    Added the Processor class to the OOP.composition package.

---
+++ src/OOP/composition/Processor.java
@@ -0,0 +1,14 @@
+package OOP.composition;
+
+public class Processor {
+
+    private String brand;
+    private String series;
+    private int generation;
+    private int cores;
+    private int threads;
+    private String cacheMemory;
+    private String frequency;
+    private String minFrequency;
+    private String maxFrequency;
+}
