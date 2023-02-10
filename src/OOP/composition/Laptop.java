d80690236f3e87ad43770fc55938d12a3dc38d67
Wandonium <hillarywando@gmail.com>
2023-02-10 05:48:07 +0300

    Added the Laptop class in the OOP.composition package.

--- src/OOP/composition/Laptop.java
+++ src/OOP/composition/Laptop.java
@@ -1,4 +1,12 @@
 package OOP.composition;
 
 public class Laptop {
+
+    private String screen;
+    private Processor processor;
+    private String ram;
+    private String hardDrive;
+    private GraphicsCard graphicsCard;
+    private String opticalDrive;
+    private String keyboard;
 }
