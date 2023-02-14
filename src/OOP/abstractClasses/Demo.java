6f0c8165784dd2330d98824f4b17e1058f5cb846
Wandonium <hillarywando@gmail.com>
2023-02-14 06:06:54 +0300

    Added the Demo class to the abstractClasses package to illustrate use of abstract classes in Java.

---
+++ src/OOP/abstractClasses/Demo.java
@@ -0,0 +1,14 @@
+package OOP.abstractClasses;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        Person john = new Vegan();
+        john.speak();
+        john.eat();
+        System.out.println("*********************");
+        Person mia = new NonVegan();
+        mia.speak();
+        mia.eat();
+    }
+}
