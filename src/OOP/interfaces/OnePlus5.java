5d6d65c7cbf74d938d6acdc4eebcb0988b41a0e4
Wandonium <hillarywando@gmail.com>
2023-02-14 05:48:20 +0300

    Made the OnePlus5 class implement the Phone interface.

--- src/OOP/interfaces/OnePlus5.java
+++ src/OOP/interfaces/OnePlus5.java
@@ -1,7 +1,18 @@
 package OOP.interfaces;
 
-public class OnePlus5 {
-    public int processor() {
-        return 835;
+public class OnePlus5 implements Phone {
+    @Override
+    public String processor() {
+        return "SD835";
+    }
+
+    @Override
+    public String os() {
+        return "Android";
+    }
+
+    @Override
+    public int spaceInGB() {
+        return 64;
     }
 }
