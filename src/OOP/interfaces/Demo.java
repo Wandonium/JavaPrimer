d8d86d1996d65eab56c2b29ee880585e5a79a425
Wandonium <hillarywando@gmail.com>
2023-02-14 05:55:29 +0300

    Added code to illustrate polymorphism using interfaces in the Demo class of the interfaces package.

--- src/OOP/interfaces/Demo.java
+++ src/OOP/interfaces/Demo.java
@@ -3,7 +3,10 @@ package OOP.interfaces;
 public class Demo {
 
     public static void main(String[] args) {
-        OnePlus5 phone = new OnePlus5();
+        // Polymorphism using interfaces.
+        Phone phone = new OnePlus5();
         System.out.println("Processor: " + phone.processor());
+        Phone phone2 = new Iphone8();
+        System.out.println("Processor: " + phone2.processor());
     }
 }
