937e355fbd9396149802cf250c99033065ff0ba8
Wandonium <hillarywando@gmail.com>
2023-02-14 06:01:18 +0300

    Added the Person class in the abstractClasses package.

---
+++ src/OOP/abstractClasses/Person.java
@@ -0,0 +1,8 @@
+package OOP.abstractClasses;
+
+public abstract class Person {
+    public void speak() {
+        System.out.println("Share his/her thoughts");
+    }
+    public abstract void eat();
+}
