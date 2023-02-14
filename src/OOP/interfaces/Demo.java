4a223032bc9977685dc56dd01c76f0e927a3fef9
Wandonium <hillarywando@gmail.com>
2023-02-14 05:42:41 +0300

    Added the OnePlus5 class and the Demo class to the interfaces package.

---
+++ src/OOP/interfaces/Demo.java
@@ -0,0 +1,9 @@
+package OOP.interfaces;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        OnePlus5 phone = new OnePlus5();
+        System.out.println("Processor: " + phone.processor());
+    }
+}
---
+++ src/OOP/interfaces/OnePlus5.java
@@ -0,0 +1,7 @@
+package OOP.interfaces;
+
+public class OnePlus5 {
+    public int processor() {
+        return 835;
+    }
+}
