c404aeca407f20735f32b5a6b00cb6f8e0dd2deb
Wandonium <hillarywando@gmail.com>
2023-02-16 20:36:41 +0300

    Used Door2 in the Shop class in the InnerClasses package along with command line arguments in 'Edit Configurations'.

--- src/OOP/InnerClasses/Shop.java
+++ src/OOP/InnerClasses/Shop.java
@@ -7,5 +7,14 @@ public class Shop {
         door.shopStatus();
         door.getLock().setLock(false);
         door.shopStatus();
+
+        Door2 door2 = new Door2();
+        //System.out.println(door2.isLocked("qwerty"));
+        //System.out.println(door2.isLocked("qwerty2"));
+        if(door2.isLocked(args[0])) {
+            System.out.println("Shop is closed! Come back later.");
+        } else {
+            System.out.println("Welcome, we are open!");
+        }
     }
 }
