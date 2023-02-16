c79cdb2c657d2f05213eba3b57809053e8ff29d4
Wandonium <hillarywando@gmail.com>
2023-02-16 20:08:15 +0300

    Added overridden methods breathe() and message() from interfaces IsAlive and LiveLife in the Person abstractClass.

--- src/OOP/abstractClasses/Person.java
+++ src/OOP/abstractClasses/Person.java
@@ -4,5 +4,16 @@ public abstract class Person implements IsAlive, LiveLife {
     public void speak() {
         System.out.println("Share his/her thoughts");
     }
+
+    @Override
+    public void breathe() {
+
+    }
+
+    @Override
+    public void message() {
+
+    }
+
     public abstract void eat();
 }
