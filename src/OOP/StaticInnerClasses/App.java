539973acace58469ed169b8e07cc1757f8302b7b
Wandonium <hillarywando@gmail.com>
2023-02-17 05:51:12 +0300

    Added code to test a static variable inside a static inner class in the OOP.StaticInnerClasses.App class.

--- src/OOP/StaticInnerClasses/App.java
+++ src/OOP/StaticInnerClasses/App.java
@@ -3,5 +3,6 @@ package OOP.StaticInnerClasses;
 public class App {
     public static void main(String[] args) {
         Outer.Inner.testing();
+        System.out.println(Outer.Inner.var);
     }
 }
--- src/OOP/StaticInnerClasses/Outer.java
+++ src/OOP/StaticInnerClasses/Outer.java
@@ -2,6 +2,7 @@ package OOP.StaticInnerClasses;
 
 public class Outer {
     public static class Inner {
+        public static String var = "Testing inner class static variable";
         public static void testing() {
             System.out.println("Testing inner class method");
         }
