0ebc22b4553c7a071397c357501a434a20cd5e70
Wandonium <hillarywando@gmail.com>
2023-02-17 05:47:51 +0300

    Added the Outer class in the StaticInnerClasses package and tested it using the App class.

--- src/OOP/StaticInnerClasses/App.java
+++ src/OOP/StaticInnerClasses/App.java
@@ -2,6 +2,6 @@ package OOP.StaticInnerClasses;
 
 public class App {
     public static void main(String[] args) {
-
+        Outer.Inner.testing();
     }
 }
---
+++ src/OOP/StaticInnerClasses/Outer.java
@@ -0,0 +1,9 @@
+package OOP.StaticInnerClasses;
+
+public class Outer {
+    public static class Inner {
+        public static void testing() {
+            System.out.println("Testing inner class method");
+        }
+    }
+}
