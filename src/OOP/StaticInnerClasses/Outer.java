55ba374b0f53610b6489433ed179fa1b9eedbd18
Wandonium <hillarywando@gmail.com>
2023-02-17 05:56:43 +0300

    Added code to test the use of the static variables and methods of an outer class in an inner static class in OOP.StaticInnerClasses.Outer class.

--- src/OOP/StaticInnerClasses/Outer.java
+++ src/OOP/StaticInnerClasses/Outer.java
@@ -1,10 +1,17 @@
 package OOP.StaticInnerClasses;
 
 public class Outer {
+
+    private static int testVar = 5;
+    private static void testOuterMethod() {
+        System.out.println("From outer method");
+    }
     public static class Inner {
         public static String var = "Testing inner class static variable";
         public static void testing() {
             System.out.println("Testing inner class method");
+            System.out.println("Outer variable test: " + testVar);
+            testOuterMethod();
         }
     }
 }
