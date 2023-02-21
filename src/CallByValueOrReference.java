2cc3002914d7bb6f1bebdea9343c3719344dff90
Wandonium <hillarywando@gmail.com>
2023-02-21 05:52:03 +0300

    Added comments in CallByValueOrReference.java class to illustrate the call by value case.

--- src/CallByValueOrReference.java
+++ src/CallByValueOrReference.java
@@ -1,10 +1,18 @@
 public class CallByValueOrReference {
 
     public static void main(String[] args) {
-        displayValue(10);
+        // Notice that var is passed to the function by value
+        // rather than by reference. Hence, it doesn't change in
+        // the main method even though it changed in the displayValue
+        // method.
+        int var = 10;
+        System.out.println("var before method call: " + var);
+        displayValue(var);
+        System.out.println("var after method call: " + var);
     }
 
-    static void displayValue(int value) {
-        System.out.println("value: " + value);
+    static void displayValue(int var) {
+        var += 10;
+        System.out.println("var inside method: " + var);
     }
 }
