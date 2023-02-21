140e8127f1b88bc28b55ac8b4d186342f847a163
Wandonium <hillarywando@gmail.com>
2023-02-21 05:44:52 +0300

    Added the CallByValueOrReference.java class to illustrate calling by value and by reference concept in Java.

---
+++ src/CallByValueOrReference.java
@@ -0,0 +1,10 @@
+public class CallByValueOrReference {
+
+    public static void main(String[] args) {
+        displayValue(10);
+    }
+
+    static void displayValue(int value) {
+        System.out.println("value: " + value);
+    }
+}
