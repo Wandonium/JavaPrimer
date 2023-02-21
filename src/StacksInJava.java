a5a85dde62f55abdcd7d2d571970692e7ebe0f22
Wandonium <hillarywando@gmail.com>
2023-02-21 20:36:36 +0300

    Made use of the stack methods: push, peek and pop in the StacksInJava.java class.

--- src/StacksInJava.java
+++ src/StacksInJava.java
@@ -8,5 +8,12 @@ public class StacksInJava {
             demo.push(i);
         }
         System.out.println("demo: " + demo);
+        // using peek method to check the last element
+        // in the stack without popping it.
+        System.out.println("peek: " + demo.peek());
+        // using pop to remove last element coz its LIFO
+        System.out.println("pop: " + demo.pop());
+        // stack after pop operation
+        System.out.println("after pop: " + demo);
     }
 }
