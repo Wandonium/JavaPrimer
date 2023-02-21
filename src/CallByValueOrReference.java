193ec440a573fa487815ae55236dd0e1be4c3e6f
Wandonium <hillarywando@gmail.com>
2023-02-21 06:08:27 +0300

    Added code to illustrate passing by reference in the CallByValueOrReference.java class.

--- src/CallByValueOrReference.java
+++ src/CallByValueOrReference.java
@@ -9,10 +9,26 @@ public class CallByValueOrReference {
         System.out.println("var before method call: " + var);
         displayValue(var);
         System.out.println("var after method call: " + var);
+
+        // Notice that names is passed to the function by reference
+        // rather than by value. Hence, changing the names[0] value in
+        // the method also changes it in the main method.
+        String[] names = {"Chaand", "John", "James"};
+        System.out.println("First name before method call: " + names[0]);
+        displayNames(names);
+        System.out.println("First name after method call: " + names[0]);
     }
 
     static void displayValue(int var) {
         var += 10;
         System.out.println("var inside method: " + var);
+    }
+
+    static void displayNames(String[] arr) {
+        arr[0] = "Peter";
+        System.out.println("Names inside method:");
+        for(String name: arr) {
+            System.out.println(name);
+        }
     }
 }
