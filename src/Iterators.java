1beeaffba989a26b2ab488e07354767b15d364c2
Wandonium <hillarywando@gmail.com>
2023-02-21 20:54:01 +0300

    Added code to illustrate the use of iterators in Java in the Iterators.java class.

--- src/Iterators.java
+++ src/Iterators.java
@@ -1,6 +1,21 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Iterators {
 
     public static void main(String[] args) {
+        List<String> countries = new LinkedList<>();
+        countries.add("Kenya");
+        countries.add("Uganda");
+        countries.add("Tanzania");
+        printList(countries);
+    }
 
+    public static void printList(List<String> list) {
+        Iterator<String> iterator = list.iterator();
+        while(iterator.hasNext()) {
+            System.out.println("Element: " + iterator.next());
+        }
     }
 }
