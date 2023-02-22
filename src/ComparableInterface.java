d646cec350b38da61b753692a0d7dcab200b5e4c
Wandonium <hillarywando@gmail.com>
2023-02-22 20:58:06 +0300

    Added the Data inner class to the ComparableInterface.java class.

--- src/ComparableInterface.java
+++ src/ComparableInterface.java
@@ -3,4 +3,23 @@ public class ComparableInterface {
     public static void main(String[] args) {
 
     }
+
+    static class Data<T> {
+        private T var;
+
+        public Data(T var) {
+            this.var = var;
+        }
+
+        public T getVar() {
+            return var;
+        }
+
+        @Override
+        public String toString() {
+            return "Data{" +
+                    "var=" + var +
+                    '}';
+        }
+    }
 }
