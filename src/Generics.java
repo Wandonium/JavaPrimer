0c0a83347da65ef546b2e3128e05cdd2e641d355
Wandonium <hillarywando@gmail.com>
2023-02-22 05:50:20 +0300

    Added the GenericClass inner class to the Generics.java class to illustrate the use of generics in Java and avoid type casting.

--- src/Generics.java
+++ src/Generics.java
@@ -2,9 +2,34 @@ public class Generics {
 
     public static void main(String[] args) {
         // use of the Data class with type casting
+        // no use of generics
         Data data = new Data("Some String");
         String var = (String) data.getObj();
         System.out.println("var: " + var);
+
+        // using generics
+        GenericClass<String> genericClass = new GenericClass<String>("Some data");
+        String genericData = genericClass.getData();
+        System.out.println("genericData: " + genericData);
+    }
+
+    static class GenericClass<T> {
+        private T data;
+
+        public GenericClass(T data) {
+            this.data = data;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        @Override
+        public String toString() {
+            return "GenericClass{" +
+                    "data=" + data +
+                    '}';
+        }
     }
 
     static class Data {
