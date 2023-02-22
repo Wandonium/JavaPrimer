878957dcbf847631f49405692613235c731a1126
Wandonium <hillarywando@gmail.com>
2023-02-22 21:03:18 +0300

    Added code to illustrate the use of the Comparable interface along with generics in the ComparableInterface.java class.

--- src/ComparableInterface.java
+++ src/ComparableInterface.java
@@ -1,10 +1,12 @@
 public class ComparableInterface {
 
     public static void main(String[] args) {
-
+        Data<Integer> data = new Data<Integer>(3);
+        System.out.println("using compareTo: "
+                + data.compareTo(2));
     }
 
-    static class Data<T> {
+    static class Data<T extends Comparable> implements Comparable<T>{
         private T var;
 
         public Data(T var) {
@@ -20,6 +22,11 @@ public class ComparableInterface {
             return "Data{" +
                     "var=" + var +
                     '}';
+        }
+
+        @Override
+        public int compareTo(T o) {
+            return getVar().compareTo(o);
         }
     }
 }
