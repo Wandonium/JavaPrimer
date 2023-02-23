08b27291d1cf627b9c81123358334be87a8aeff1
Wandonium <hillarywando@gmail.com>
2023-02-23 20:34:14 +0300

    Added code to show that Sets do not evaluate uppercase vs lowercase values and do not respect the ordering of values.

--- src/Collections/Sets.java
+++ src/Collections/Sets.java
@@ -5,12 +5,17 @@ import java.util.Set;
 
 public class Sets {
     public static void main(String[] args) {
-        // Sets ignore duplicate values:
+        // Sets ignore duplicate values!
+        // Also note that the order of input is not respected
+        // in the output.
         Set<String> set = new HashSet<>();
         set.add("Peter");
         set.add("John");
         set.add("James");
         set.add("Peter");
+        // Note that Sets do not ignore values with a different
+        // case i.e. lowercase vs uppercase
+        set.add("peter");
         System.out.println("set: " + set);
     }
 }
