0d121265e0e9e40662babce4f48b285a3542696c
Wandonium <hillarywando@gmail.com>
2023-02-23 20:46:38 +0300

    Added code in the Set.java class to show how a LinkedHashSet maitains the ordering of it's input values.

--- src/Collections/Sets.java
+++ src/Collections/Sets.java
@@ -1,6 +1,7 @@
 package Collections;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class Sets {
@@ -17,5 +18,19 @@ public class Sets {
         // case i.e. lowercase vs uppercase
         set.add("peter");
         System.out.println("set: " + set);
+        // No get method in sets
+        System.out.println("set contains 'peter': " + set.contains("peter"));
+
+        // HashSet returns random ordering while LinkedSet
+        // returns the input order.
+        Set<Integer> hashSet = new HashSet<>();
+        Set<Integer> linkedSet = new LinkedHashSet<>();
+        for(int i=10; i>0; i--) {
+            hashSet.add(i);
+            linkedSet.add(i);
+        }
+        System.out.println("hashSet: " + hashSet);
+        System.out.println("linkedSet: " + linkedSet);
+
     }
 }
