ff274ef89eb069f13cf09e6ef3d01ad7a8bf8c88
Wandonium <hillarywando@gmail.com>
2023-02-23 21:09:27 +0300

    Added code to show the use of TreeSets with Objects.

--- src/Collections/Sets.java
+++ src/Collections/Sets.java
@@ -1,9 +1,6 @@
 package Collections;
 
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Sets {
     public static void main(String[] args) {
@@ -42,5 +39,69 @@ public class Sets {
         treeSet.add("Truck");
         System.out.println("treeSet: " + treeSet);
 
+        // Sets do not ignore duplicate objects:
+        ObjectSet();
+
+        // TreeSet with objects and Comparator
+        ObjectTreeSet();
+    }
+
+    public static void ObjectSet() {
+        Set<Data> set = new HashSet<>();
+        set.add(new Data(1, "one"));
+        set.add(new Data(2, "two"));
+        set.add(new Data(3, "three"));
+        set.add(new Data(3, "three"));
+        // Notice how we do not ignore the duplicated new Data(3, "three")
+        // because this is an object which has a different memory location
+        System.out.println("set: " + set);
+    }
+
+    public static void ObjectTreeSet() {
+        // This causes an error because the Data class does not have a
+        // compareTo method to be used by the TreeSet to sort the values
+        //Set<Data> set = new TreeSet<>();
+
+        Comparator<Data<Integer, String>> compareKey = new Comparator<Data<Integer, String>>() {
+            @Override
+            public int compare(Data<Integer, String> o1, Data<Integer, String> o2) {
+                if(o1.getKey() > o2.getKey()) {
+                    return 1;
+                } else if(o1.getKey() < o2.getKey()) {
+                    return -1;
+                } else return 0;
+            }
+        };
+        Set<Data<Integer, String>> set = new TreeSet<>(compareKey);
+        set.add(new Data(3, "three"));
+        set.add(new Data(1, "one"));
+        set.add(new Data(2, "two"));
+        System.out.println("treeSet: " + set);
+    }
+
+    static class Data<K,V> {
+        private K key;
+        private V value;
+
+        public Data(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return "Data{" +
+                    "key=" + key +
+                    ", value=" + value +
+                    '}';
+        }
     }
 }
