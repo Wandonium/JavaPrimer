b4f394e2a5386947ac6c3edc5a42cb15292d66d3
Wandonium <hillarywando@gmail.com>
2023-02-27 21:01:44 +0300

    Added an example of a for-each loop when dealing with Maps in Java.

--- src/Collections/Maps.java
+++ src/Collections/Maps.java
@@ -6,9 +6,14 @@ import java.util.Map;
 public class Maps {
     public static void main(String[] args) {
         Map<Integer, String> map = new HashMap<>();
-        map.put(1, "one");
-        map.put(2, "two");
-        map.put(3, "three");
-        System.out.println("map: " + map);
+        map.put(3, "Peter");
+        map.put(1, "John");
+        map.put(2, "James");
+        // HashMap does not guarantee the sorting of the entries
+        System.out.println("unsorted map: " + map);
+        // for-each loop when dealing with maps:
+        for(Map.Entry<Integer, String> entry: map.entrySet()) {
+            System.out.println("key: " + entry.getKey() + " value: " + entry.getValue());
+        }
     }
 }
