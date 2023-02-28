91d492071ce2c4fce078e64c9bca7164a9924b99
Wandonium <hillarywando@gmail.com>
2023-02-28 06:00:12 +0300

    Added code to show use of HashMap and TreeMap with custom objects in Java.

--- src/Collections/Maps.java
+++ src/Collections/Maps.java
@@ -3,11 +3,13 @@ package Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Maps {
     public static void main(String[] args) {
         //mapLooping();
-        mapDuplication();
+        //mapDuplication();
+        objectMaps();
     }
 
     public static void mapLooping() {
@@ -43,5 +45,52 @@ public class Maps {
         map.put(3, "three");
         // notice the lack of duplication for keys
         System.out.println("map: " + map);
+    }
+
+    public static void objectMaps() {
+        //Map<Code, String> map = new HashMap<>();
+        Map<Code, String> map = new TreeMap<>();
+        map.put(new Code("S01", "L01"), "Generics");
+        map.put(new Code("S01", "L02"), "OOP");
+        map.put(new Code("S02", "L01"), "Network Programming");
+        // notice that duplicates are not removed when the map is a HashMap
+        // but are removed when it's a TreeMap
+        map.put(new Code("S01", "L01"), "Collections");
+        for(Code key: map.keySet()) {
+            System.out.println("key: " + key + " value: " + map.get(key));
+        }
+    }
+
+    static class Code implements Comparable<Code> {
+        private String sectionNo;
+        private String lectureNo;
+
+        public Code(String sectionNo, String lectureNo) {
+            this.sectionNo = sectionNo;
+            this.lectureNo = lectureNo;
+        }
+
+        public String getSectionNo() {
+            return sectionNo;
+        }
+
+        public String getLectureNo() {
+            return lectureNo;
+        }
+
+        @Override
+        public String toString() {
+            return "Code{" +
+                    "sectionNo='" + sectionNo + '\'' +
+                    ", lectureNo='" + lectureNo + '\'' +
+                    '}';
+        }
+
+        @Override
+        public int compareTo(Code o) {
+            String code1 = this.sectionNo.concat(this.lectureNo);
+            String code2 = o.sectionNo.concat(o.lectureNo);
+            return code1.compareTo(code2);
+        }
     }
 }
