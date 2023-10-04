67745ed236009954eeed2955d45c977dbf76804d
Wandonium <hillarywando@gmail.com>
2023-10-04 15:37:07 +0300

    Use lambda expressions only with functional interfaces.

--- src/LambdaExpressions/Lambdas1.java
+++ src/LambdaExpressions/Lambdas1.java
@@ -1,7 +1,11 @@
 package LambdaExpressions;
 
+
 interface Lambda {
     public void demo();
+    // We can use lambda expressions only with functional interfaces
+    // i.e interfaces with a single function.
+    // public void demo2(int x);
 }
 
 public class Lambdas1 {
@@ -16,7 +20,15 @@ public class Lambdas1 {
         };
         lambda.demo();
 
+        // lambda expression with a single statement.
         Lambda lambda2 = ()->System.out.println("Inside demo method of Lambda interface using lambda expression...");
         lambda2.demo();
+
+        // lambda expression with multiple statements.
+        Lambda lambda3 = ()-> {
+            System.out.println("Inside demo method 1...");
+            System.out.println("Inside demo method 2...");
+        };
+        lambda3.demo();
     }    
 }
